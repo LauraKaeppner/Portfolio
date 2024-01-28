090bef1d54c68bb698ba33f790716b82ed2bb592
Dennis Hawran <56675253+AtmoGD@users.noreply.github.com>
2024-01-28 18:39:03 +0100

    Added defaultCube hover logic & deleted unused css

---
+++ js/imageSwitcher.js
@@ -0,0 +1,27 @@
+let staticCube = document.querySelector('.staticCube');
+let hiddenCube = document.querySelector('.hiddenCube');
+
+let gifURL = "./defaultcube/defaultCubeuwu.gif";
+
+addEventListener('load', init);
+
+function init () {
+    staticCube.addEventListener('mouseover', showHiddenCube);
+    hiddenCube.addEventListener('mouseout', showStaticCube);
+    
+    showStaticCube();
+}
+
+function showStaticCube () {
+    staticCube.style.display = 'block';
+    hiddenCube.style.display = 'none';
+}
+
+function showHiddenCube () {
+    staticCube.style.display = 'none';
+    hiddenCube.style.display = 'block';
+
+    let gifImg = document.getElementById('gifImg');
+    gifImg.src = "";
+    gifImg.src = gifURL;
+}
